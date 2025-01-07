a1fb815d0dde55b261cb51ec459280a5696190f5
YoannSME <115040784+YoannSME@users.noreply.github.com>
2025-01-08 00:35:08 +0100

    Delete Dev directory

--- Dev/tst_config.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "config.h"
-
-int main(){
-    printf("\nTest module config\n\n");
-    return 0;
-}
--- Dev/tst_deplacement.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "deplacement.h"
-
-int main(){
-    printf("\nTest module deplacement\n\n");
-    return 0;
-}
